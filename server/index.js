dea7203cd648e7686eff3083bf96f792c46ff310
Amit Klein <amitkl@welldone-software.com>
2022-01-22 14:48:49 +0200

    added dotenv

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
+
 const postRouter = require('./routes/posts');
 
 const app = express();
@@ -12,9 +14,6 @@ app.use(cors());
 
 app.use('/posts', postRouter);
 
-const CONNECTION_URL = 'mongodb+srv://stam123:stam123@cluster0.xsgi4.mongodb.net/memories?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 4000;
-
-mongoose.connect(CONNECTION_URL)
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose.connect(process.env.CONNECTION_URL)
+  .then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
